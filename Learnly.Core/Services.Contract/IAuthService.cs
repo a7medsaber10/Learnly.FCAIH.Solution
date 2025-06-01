bc3db384d792089cbb44b6657c68271a2dc4565c
Ahmed 20210063 <Ahmed_20210063@fci.helwan.edu.eg>
2025-06-01 16:38:54 +0300

    Adding AuthService

---
+++ Learnly.Core/Services.Contract/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Learnly.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnly.Core.Services.Contract
+{
+    public interface IAuthService
+    {
+        Task<string> CreateTokenAsync(AppUser user, UserManager<AppUser> userManager);
+    }
+}
